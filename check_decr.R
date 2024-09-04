35bdca211b17f9dee1e990603baf423967d14d9f
gibo <gibo.gaf@gmail.com>
2024-09-04 08:34:35 +0200

    add git commands

--- check_decr.R
+++ check_decr.R
@@ -10,3 +10,19 @@ build()
 check_man()
 check()
 
+
+# git
+# git remote add github git@github.com:gibonet/decr.git
+
+# git remote -v
+# github	git@github.com:gibonet/decr.git (fetch)
+# github	git@github.com:gibonet/decr.git (push)
+# origin	git@gitlab.com:gibonet/decr.git (fetch)
+# origin	git@gitlab.com:gibonet/decr.git (push)
+
+# origin is gitlab, github is github
+
+# Push to github and gitlab -----------
+# git push -u github master
+# git push -u origin master
+
